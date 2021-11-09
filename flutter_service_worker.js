54e6b75dcb19d56848d86b203b1d921a62918cee
UrielJG <90708434+UrielJG@users.noreply.github.com>
2021-11-08 22:07:52 -0600

    Prueba trece

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,9 +13,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1d727c3c171fb6fd18eb12c1bb4bed5d",
-"/": "1d727c3c171fb6fd18eb12c1bb4bed5d",
-"main.dart.js": "c4895d9ec25fdd00e57f26df94d5f087",
+"index.html": "37c5dff257fc3a883b736e7e08dca398",
+"/": "37c5dff257fc3a883b736e7e08dca398",
+"main.dart.js": "239da82624cf8fa79da69e22da5120b5",
 "manifest.json": "ac89e5e86cc1bafe9e6cca2c4079e9b7",
 "version.json": "c4a06e0094a124367fbfd607044de3a6"
 };
